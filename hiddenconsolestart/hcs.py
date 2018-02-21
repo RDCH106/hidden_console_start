a647d0b8b7b2cebe28e4e0eded05c76caa7c6818
Rubén de Celis Hernández <rdecelis@stt-systems.com>
2018-02-21 13:38:11 +0100

    Add HCS structure

---
+++ hiddenconsolestart/hcs.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from hiddenconsolestart.metadata import Metadata
+
+
+class HCS(object):
+    def __init__(self):
+        self.meta = Metadata()
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    hcs = HCS()
+    hcs.run()
