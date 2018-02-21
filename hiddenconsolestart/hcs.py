da31aa8b30eafb37c9c56a8caa0a8871e79e9744
Rubén de Celis Hernández <rdecelis@stt-systems.com>
2018-02-21 16:25:49 +0100

    Add arguments processing

--- hiddenconsolestart/hcs.py
+++ hiddenconsolestart/hcs.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from hiddenconsolestart.metadata import Metadata
+from hiddenconsolestart.subprocess import HCSProcess
+import argparse
+import threading
 
 
 class HCS(object):
     def __init__(self):
         self.meta = Metadata()
+        self.__threads = list()
 
+        # Parse arguments provided
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-v', '--version', action='version', version=self.meta.get_version())
+        parser.add_argument('-e', '--execute', dest='execute',
+                            help='Executables to launch (can be a space seperated list)',
+                            metavar='S', type=str, nargs='+')
+        
     def run(self):
         pass
 
